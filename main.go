ce65eaeaa32a1346e8275febde4fbf0e7529d45d
Michael Bang <michael@katnegermis.dk>
2015-06-29 23:43:23 +0200

    Update cookie's MaxAge, and refactor magic strings.

--- main.go
+++ main.go
@@ -14,19 +14,24 @@ type CookieResponse struct {
 	Success    bool
 }
 
+const (
+	cookieNameKey = "cookie_name"
+)
+
 func setCookie(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		log.Println(err)
 	}
-	cName := r.FormValue("cookie_name")
-	c := http.Cookie{
+	cName := r.FormValue(cookieNameKey)
+	maxAge := 10
+	c := &http.Cookie{
 		Name:    cName,
 		Value:   "I'm a unique id!", // Probably should be a random string verified during checkcookie.
-		MaxAge:  10,
+		MaxAge:  maxAge,
 		Domain:  r.URL.Host,
 		Expires: time.Now().Add(time.Duration(10 * time.Second)),
 	}
-	http.SetCookie(w, &c)
+	http.SetCookie(w, c)
 	marshalAndWrite(w, CookieResponse{CookieName: cName, Success: true})
 }
 
@@ -34,8 +39,12 @@ func checkCookie(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		log.Println(err)
 	}
-	cName := r.FormValue("cookie_name")
+	cName := r.FormValue(cookieNameKey)
 	c, err := r.Cookie(cName)
+	if err == nil && c != nil {
+		c.MaxAge = -1
+		http.SetCookie(w, c)
+	}
 	resp := CookieResponse{CookieName: cName, Success: true}
 	if err != nil || c == nil {
 		resp.Success = false
