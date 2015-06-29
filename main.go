f5a8beaa367fe3712b4f39823a924c3856b912cc
Michael Bang <michael@katnegermis.dk>
2015-06-29 18:29:01 +0200

    Initial commit.

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type CookieResponse struct {
+	CookieName string
+	Success    bool
+}
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Println(err)
+	}
+	cName := r.FormValue("cookie_name")
+	c := http.Cookie{
+		Name:    cName,
+		Value:   "I'm a unique id!", // Probably should be a random string verified during checkcookie.
+		MaxAge:  10,
+		Domain:  r.URL.Host,
+		Expires: time.Now().Add(time.Duration(10 * time.Second)),
+	}
+	http.SetCookie(w, &c)
+	marshalAndWrite(w, CookieResponse{CookieName: cName, Success: true})
+}
+
+func checkCookie(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Println(err)
+	}
+	cName := r.FormValue("cookie_name")
+	c, err := r.Cookie(cName)
+	resp := CookieResponse{CookieName: cName, Success: true}
+	if err != nil || c == nil {
+		resp.Success = false
+	}
+	if err = marshalAndWrite(w, resp); err != nil {
+		log.Println(err)
+	}
+}
+
+func marshalAndWrite(w http.ResponseWriter, data interface{}) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	total := 0
+	for total < len(b) {
+		i, err := w.Write(b)
+		if err != nil {
+			return err
+		}
+		total += i
+	}
+	return nil
+}
+
+func main() {
+	port := flag.Int("port", 8080, "Port to listen for connections on.")
+	flag.Parse()
+
+	http.HandleFunc("/checkcookie", checkCookie)
+	http.HandleFunc("/setcookie", setCookie)
+	fmt.Printf("Starting server on port %d..\n", *port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
+		log.Fatal(err)
+	}
+}
